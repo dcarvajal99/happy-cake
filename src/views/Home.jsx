776aea7fa70f4818f74bf21170232fa08007acc2
Diego Carvajal MTrabajo <diego.carvajal.1515@gmail.com>
2023-03-17 00:50:53 -0300

    up

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -7,7 +7,7 @@ const Home = () =>{
         <>
         <Container className="mx-auto">
             <Container className="my-5 text-center">
-                <h1>Bienvenido a Happy Cake</h1>
+                <h1>Bienvenido a Happy Cake :)</h1>
                 <p> El lugar de los pasteles felices</p>
                 <hr/>
             </Container>
